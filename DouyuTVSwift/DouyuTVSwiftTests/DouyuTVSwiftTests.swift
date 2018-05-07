2ef9a233e3c315b28266a17eefb3d2a144216066
Zzzxf <452681917@qq.com>
2018-05-07 11:52:27 +0800

    InitialV

---
+++ DouyuTVSwift/DouyuTVSwiftTests/DouyuTVSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  DouyuTVSwiftTests.swift
+//  DouyuTVSwiftTests
+//
+//  Created by  Zzzxf on 07/05/2018.
+//  Copyright © 2018 goodDay. All rights reserved.
+//
+
+import XCTest
+@testable import DouyuTVSwift
+
+class DouyuTVSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
